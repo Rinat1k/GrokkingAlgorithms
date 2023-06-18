12f3f54115125c0c3640ca1a38301c1001055d8e
Rinat1k <roma.tankin@gmail.com>
2023-06-18 21:57:11 +0200

    Implemented binary search

--- src/main/java/alghorithms/BinarySearch.java
+++ src/main/java/alghorithms/BinarySearch.java
@@ -1,2 +1,27 @@
-package alghorithms;public class BinarySearch {
+package alghorithms;
+
+import java.util.Arrays;
+
+// O(log2n)
+public class BinarySearch {
+
+    public int run(int[] data, int target) {
+        int mid, guess;
+        int beg = 0;
+        int end = data.length - 1;
+
+        while (beg <= end) {
+            mid = (beg + end) / 2;
+            guess = data[mid];
+
+            if (target == guess) return mid;
+            if (target > guess) {
+                beg = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return 0;
+    }
+
 }
--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -1,2 +1,17 @@
+import alghorithms.BinarySearch;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BinarySearchTest {
+
+    private final BinarySearch binarySearch = new BinarySearch();
+
+    @Test
+    public void elementExistTest() {
+        int[] data = {0, 24, 26, 34, 35, 41, 41, 42, 42, 51, 60, 66, 67, 67, 70, 72, 73, 75, 84, 84, 86, 89, 95};
+        int target = 73;
+
+        Assert.assertEquals("Algorithm not working", 16, binarySearch.run(data, target));
+    }
+
 }
